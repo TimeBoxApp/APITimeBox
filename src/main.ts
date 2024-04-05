b050303dd7e4e27e0ec9399b291b94fea8d7f148
Roman Kulyk <roman.kulyk@romankulyk.local>
2024-04-05 18:07:32 +0300

    Add origin to cors

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,13 @@ async function bootstrap() {
 
   app.use(helmet());
   app.enableCors({
-    origin: ['http://localhost:8001', 'http://localhost:3001', 'https://timebox.guru/', 'https://api.timebox.guru/'],
+    origin: [
+      'http://localhost:8001',
+      'http://localhost:3001',
+      'https://timebox.guru/',
+      'https://www.timebox.guru/',
+      'https://api.timebox.guru/'
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true
   });
